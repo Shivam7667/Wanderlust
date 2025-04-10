e5688af7698926383702210a403b594de0000c34
Shivam7667 <shivam.jha_cs22@gla.ac.in>
2025-04-10 21:17:20 +0530

    commit

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) =>({...obj, owner: '67f3c7a9ec733e6f0185a09f'}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
